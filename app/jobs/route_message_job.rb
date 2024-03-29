1fa340dc7fea446d966b943fe8e70d929da438e2
Arturo Guzman <ing.arturo.guzman@gmail.com>
2024-03-29 13:11:28 -0400

    chore: refactor route message job

--- app/jobs/route_message_job.rb
+++ app/jobs/route_message_job.rb
@@ -4,38 +4,42 @@ class RouteMessageJob < ApplicationJob
   NoMessagePartFound = Class.new(StandardError)
 
   def perform(message_uid)
-    find_and_set_message(message_uid)
-    route_message_to_mailboxes
-    delete_message
+    find_mail_depot_message(message_uid)
+    route_mail_depot_message_to_mailboxes
+    delete_mail_depot_message
   end
 
   private
 
   attr_reader :message
 
-  def find_and_set_message(message_uid)
+  def find_mail_depot_message(message_uid)
     @message = MailDepot::Client.instance.find_message(message_uid)
     raise "Message with uid #{message_uid} not found" if message.nil?
 
     logger.info "Routing message from #{message.from} to #{message.to} with subject: #{message.subject}"
   end
 
-  def route_message_to_mailboxes
-    Mailbox.where(email: recipient_emails).find_each do |mailbox|
+  def route_mail_depot_message_to_mailboxes
+    recipients_mailboxes.find_each do |mailbox|
       mailbox_sender = mailbox.mailbox_senders.create_or_find_by!(email: sender_email)
       mailbox_message = create_mailbox_message(mailbox_sender)
       ParseMessageJob.perform_later(mailbox_message) if mailbox_sender.allowed?
     rescue NoMessagePartFound
-      msg_details = "message #{message_uid} from #{message.from} with subject: #{message.subject}"
-      logger.error "No message part found for #{msg_details}"
+      logger.error "No message parts found: #{message_details}"
     end
   end
 
-  def delete_message
+  def recipients_mailboxes
+    Mailbox.where(email: recipients_emails)
+  end
+
+  def delete_mail_depot_message
+    notify_message_dropped if recipients_mailboxes.empty?
     MailDepot::Client.instance.delete_message(message.uid)
   end
 
-  def recipient_emails
+  def recipients_emails
     recipients = message.to
     recipients += message.cc if message.cc.present?
     recipients += message.bcc if message.bcc.present?
@@ -68,5 +72,13 @@ class RouteMessageJob < ApplicationJob
 
   def decode_body(msg_part)
     msg_part.decode_body.force_encoding("UTF-8").encode("UTF-8", invalid: :replace, undef: :replace, replace: "⍰")
+  end
+
+  def notify_message_dropped
+    logger.info "Message dropped: #{message_details}"
+  end
+
+  def message_details
+    "Message #{message_uid} from #{message.from} with subject: #{message.subject}"
   end
 end
