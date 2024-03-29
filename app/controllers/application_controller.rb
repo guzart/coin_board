6b4806eb72ce8bd8fe4f94419cf43c6fd52deb27
Arturo Guzman <ing.arturo.guzman@gmail.com>
2024-03-29 13:35:39 -0400

    chore: add current_user_root_path helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include Authentication
+
+  helper_method :current_user_root_path
+
+  private
+
+  def current_user_root_path
+    mailbox_path(current_user.mailbox)
+  end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   allow_anonymous_access!
 
   def index
+    redirect_to current_user_root_path if current_user
   end
 end
