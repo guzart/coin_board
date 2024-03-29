37c647993659f0238a350ea44bc5201b39d18f5f
Arturo Guzman <ing.arturo.guzman@gmail.com>
2024-03-29 14:26:52 -0400

    feat(components): add list group and status badge components

---
+++ app/components/list_group_component.rb
@@ -0,0 +1,16 @@
+class ListGroupComponent < ApplicationComponent
+  renders_many :items, lambda { |content = nil, &block|
+    content_tag :li, content, class: "list-group-item", &block
+  }
+
+  slim_template <<~SLIM
+    = content_tag :ul, class: root_class, id:, data: do
+      = content
+  SLIM
+
+  private
+
+  def root_class
+    "list-group #{class_name}"
+  end
+end
---
+++ app/components/status_badge_component.rb
@@ -0,0 +1,33 @@
+class StatusBadgeComponent < ApplicationComponent
+  param :text, optional: true
+  option :variant, default: proc { :primary }
+
+  slim_template <<~SLIM
+    = content_tag :div, class: root_class, id:, data: do
+      = content_tag :div, class: status_wrapper_class do
+        = content_tag :div, "", class: status_class
+      = content_tag :p, text_or_content, class: content_class
+  SLIM
+
+  private
+
+  def root_class
+    "d-flex align-items-center column-gap-1 #{class_name}"
+  end
+
+  def status_wrapper_class
+    "flex-grow-0 flex-shrink-0 rounded-circle bg-#{variant}-subtle p-1"
+  end
+
+  def status_class
+    "h-1 w-1 rounded-circle bg-#{variant}"
+  end
+
+  def content_class
+    "text-xs lh-base text-secondary m-0"
+  end
+
+  def text_or_content
+    text || content
+  end
+end
