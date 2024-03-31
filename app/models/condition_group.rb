90c9509fdedee19eaeedd19d48346be5c3269924
Arturo Guzman <ing.arturo.guzman@gmail.com>
2024-03-31 00:26:42 -0400

    feat(condition group): add condition evaluation logic

--- app/models/condition_group.rb
+++ app/models/condition_group.rb
@@ -20,14 +20,44 @@
 #  user_id                    (user_id => users.id)
 #
 class ConditionGroup < ApplicationRecord
-  OPERATORS = %w[AND OR].freeze
+  AND = "AND".freeze
+  OR = "OR".freeze
+
+  def self.logical_operators
+    [AND, OR]
+  end
+  public_class_method :logical_operators
 
   belongs_to :user
-  has_many :conditions, dependent: :destroy
-
   belongs_to :parent_condition_group, class_name: "ConditionGroup", optional: true
 
-  validates :logical_operator, presence: true, inclusion: { in: OPERATORS }
+  has_many :conditions, dependent: :destroy
+  has_many :child_condition_groups, class_name: "ConditionGroup", foreign_key: :parent_condition_group_id
 
-  accepts_nested_attributes_for :conditions, allow_destroy: true
+  validates :logical_operator, presence: true, inclusion: { in: logical_operators }
+
+  def satisfied_by?(instance)
+    case logical_operator
+    when AND
+      and_operator_satisfied_by?(instance)
+    when OR
+      or_operator_satisfied_by?(instance)
+    end
+  end
+
+  private
+
+  def and_operator_satisfied_by?(instance)
+    child_condition_groups.all? { |group| group.satisfied_by?(instance) } &&
+      conditions.all? do |condition|
+        condition.satisfied_by?(instance)
+      end
+  end
+
+  def or_operator_satisfied_by?(instance)
+    child_condition_groups.any? { |group| group.satisfied_by?(instance) } ||
+      conditions.any? do |condition|
+        condition.satisfied_by?(instance)
+      end
+  end
 end
