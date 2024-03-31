dfe0ea1d7050516c0f46a2fb959997dfe5a8b777
Arturo Guzman <ing.arturo.guzman@gmail.com>
2024-03-31 19:14:16 -0400

    feat(icons): add support for icon aliases

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,11 +1,23 @@
 module IconHelper
   def icon(name, animate: nil)
+    icon_name = icon_name_for_alias(name).to_s.gsub("_", "-")
     style_classname = "fa-regular"
-    icon_classname = "fa-#{name.to_s.gsub('_', '-')}"
+    icon_classname = "fa-#{icon_name}"
     content_tag(:i, nil, class: [style_classname, icon_classname, icon_animate_classname(animate)].compact.join(" "))
   end
 
   def icon_animate_classname(name)
     "fa-#{name.to_s.gsub('_', '-')}" if name
+  end
+
+  def icon_name_for_alias(icon_alias)
+    case icon_alias
+    when :delete
+      :trash
+    when :remove
+      :trash
+    else
+      icon_alias
+    end
   end
 end
