230cd208fcd0fdb4d74c8da83fa010faf96b24e1
Arturo Guzman <ing.arturo.guzman@gmail.com>
2024-03-31 20:17:33 -0400

    chore: run fetch mail job in queue

--- config/initializers/mail_depot.rb
+++ config/initializers/mail_depot.rb
@@ -1,7 +1,7 @@
 Rails.configuration.after_initialize do
   if Rails.const_defined?("Server") && ENV.fetch("MAIL_DEPOT_LISTEN_ENABLED", "false") == "true"
     MailDepot::Client.instance.detached_listen_for_mail do
-      FetchMailDepotEmailsJob.perform_now
+      FetchMailDepotEmailsJob.perform_later
     end
   end
 end
