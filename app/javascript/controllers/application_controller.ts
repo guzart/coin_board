982a814dee4d52ad077b4efbdab2e3f7bc41ffe5
Arturo Guzman <ing.arturo.guzman@gmail.com>
2024-04-08 23:29:52 -0400

    chore: ignore arguments from action definition

--- app/javascript/controllers/application_controller.ts
+++ app/javascript/controllers/application_controller.ts
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 export abstract class ApplicationController extends Controller {
   protected addActionDescriptors(
-    actions: Record<string, (args: any[]) => any>,
+    actions: Record<string, (_args: any[]) => any>,
   ) {
     const existingActions = this.element.getAttribute("data-action") || "";
     const actionDescriptors = Object.entries(actions)
